8101e57ba9a73c0becb85e05f38f623299210865
Stephen Woodruff <stephen.j.woodruff@gmail.com>
2014-02-11 12:19:47 -0500

    Created Board

--- MaKeyMeSorry/MainPage.xaml.cs
+++ MaKeyMeSorry/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace MaKeyMeSorry
         {
             this.InitializeComponent();
         }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
