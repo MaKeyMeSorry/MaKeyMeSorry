64c168fc99c61af6efbfe00d19094e794c4e3696
warnermc <warnermc@umich.edu>
2014-02-13 09:45:03 -0500

    updatd game and board to stephens branch?
    not sure i did anything, I i think you just didn't have these files yet

---
+++ MaKeyMeSorry/Board.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaKeyMeSorry
+{
+    class Board{
+
+
+        private List<Square> theBoard;
+
+        // These could be organized as a vector of vectors of Squares 
+        // with the equivalent color = to its representative enum
+        private List<Square> blueBase;
+        private List<Square> yellowBase;
+        private List<Square> greenBase;
+        private List<Square> redBase; 
+
+
+        //Again this could be a vector of int's if it seems to work better.
+        private int blueStart;
+        private int yellowStart;
+        private int greenStart;
+        private int redStart;
+
+        private static int slideSize1;
+        private static int slideSize2;
+
+        private static int numSquares;
+
+        // Not sure if we need
+        public void update_board(){
+            // TODO Write update_board
+        }
+
+        // Returns the start square number for the inputted color equivalent
+        public int get_start_square(int numColor){
+            // TODO Write get_start_square
+            return -1;
+        }
+
+        public void execute_slide(int squareNum, int pawnNum){
+            // TODO Write execute_slide
+        }
+
+        public Board(){
+            // TODO Write Board constructor
+        }
+
+
+    }
+}
---
+++ MaKeyMeSorry/Game.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaKeyMeSorry
+{
+    class Game{
+
+            // Holds the number of human players in game
+            private int numHumanPlayers;
+
+            // Make default constructor private so only 
+            // the correct constructor is used.
+            private Game() { }
+
+            //helper function for get_move_options
+            private Square get_move(Pawn pawn, Card card)
+            {
+                // TODO Write get_move
+                return null;
+            }
+
+            // Could add getters and setters if we don't want
+            // these to be public, but might be a pain to access then
+            public List<Player> players;
+            public Board board;
+            public Deck deck;
+
+            // Constructs the Game class with numHumans 
+            // representing the number of human players.
+            // Sets numHumanPlayers
+            public Game(int numHumans)
+            {
+                // TODO Write Game constructor
+            }
+
+
+            // Returns the player who has won the game.
+            // Returns NULL if there currently is no winner
+            public Player get_winner()
+            {
+                // TODO Write get_winner
+                return null;
+            }
+
+            // Returns number of human players
+            public int get_num_human_players()
+            {
+                // TODO Write get_num_human_players
+                return -1;
+            }
+
+           //this should be the meat of the entire game i thinkkkk? haha 
+            public Tuple<Pawn, List<Square>> get_move_options(Color player, Card card)
+            {
+                // TODO Write get_move_options
+                return null;
+            }
+    }
+}
