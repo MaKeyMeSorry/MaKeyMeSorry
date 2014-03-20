fd7095ccaee21dbbaa24c8a028b4557aea5f57eb
warnermc <warnermc@umich.edu>
2014-03-20 17:01:20 -0400

    trying to merge

--- MaKeyMeSorry/MainPage.xaml.cs
+++ MaKeyMeSorry/MainPage.xaml.cs
@@ -228,6 +228,7 @@ namespace MaKeyMeSorry
                 }
             
             }
+/*<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 =======
@@ -236,6 +237,9 @@ namespace MaKeyMeSorry
 =======
             // testing the combo data
 >>>>>>> a719f5762437324fae6cb912cb0ca0d7bb12a8f0
+=======
+            // testing the combo data
+>>>>>>> a719f5762437324fae6cb912cb0ca0d7bb12a8f0*/
 
         }
 
